436a5fc96d47f910f033e62f784505436ac8334c
keerthana1816 <keerthana1816@gmail.com>
2022-05-05 16:43:51 +0530

    Dress shop app

---
+++ src/main/java/com/dressshopapp/DressshopappUserMsApiApplication.java
@@ -0,0 +1,13 @@
+package com.dressshopapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DressshopappUserMsApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DressshopappUserMsApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dressshopapp/DressshopappUserMsApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dressshopapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DressshopappUserMsApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
